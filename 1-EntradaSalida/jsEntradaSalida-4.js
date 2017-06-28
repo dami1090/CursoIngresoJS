0cfdb84f7fa1e4698c8dd08512e7dec08d50cb7d
dami1090 <damihighelo@gmail.com>
2017-06-27 22:01:07 -0300

    tarea en casa 27/06/17

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,7 @@
 */
 function Mostar()
 {
-	
+	var nombre = prompt("ingrese su nombre");
+	document.getElementById("elNombre").value=nombre;
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,11 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+    var numero1 = document.getElementById("numeroUno").value;
+    var numero2 = document.getElementById("numeroDos").value;
+    numero1= parseInt(numero1);
+    numero2= parseInt(numero2);
+    var resultado= numero1+numero2;
+    alert(resultado);
 }
 
